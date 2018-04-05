8ec167508f0202479668b2337ebd5d4b895e7ad0
Rainer Wittmann <rainer.wittmann@comsysto.com>
2018-04-05 12:35:34 +0200

    add material ui button to change color of bottle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Button from 'material-ui/Button';
+import Icon from 'material-ui/Icon';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
 
-      </div>
-    );
-  }
+    render() {
+        return (
+            <MuiThemeProvider>
+                <div style={{ position: 'absolute', bottom: 5, right: 5, left: 5, display: 'flex', justifyContent: 'flex-end' }}>
+                    <Button variant={'fab'} color={'primary'} onClick={() => window.changeColor('0x' + Math.random().toString(16).slice(2, 8))}>
+                        <Icon>brush</Icon>
+                    </Button>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
